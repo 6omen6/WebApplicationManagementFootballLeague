5de74080217e5b747d4724f8edd6ece17dddf560
krozenek <konrad.rozenek@comarch.pl>
2014-10-14 15:20:48 +0200

    Poprawienie ram projekt (dodanie folderu Presentation oraz projektu AdministativePanel)

---
+++ WebApplicationManagementFootballLeague/AdministrativePanel/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AdministrativePanel
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
